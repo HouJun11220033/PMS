71377d23d3df45e77f4806da9d2b24e9cd2f4210
HouJun11220033 <1144724015@qq.com>
2016-10-28 23:33:23 +0800

    邮箱找回密码

---
+++ src/com/pms/controller/PwdFind.java
@@ -0,0 +1,18 @@
+package com.pms.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PwdFind {
+
+	// @ResponseBody
+//	public String sendEmail(HttpServletRequest request) {
+//		//DynamicParams params = new DynamicParams(request);
+//		StringBuilder builder = new StringBuilder();
+//		StringBuffer url = new StringBuffer();
+//		// String type=params
+//
+//
+//	}
+
+}
